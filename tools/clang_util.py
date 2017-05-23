74d4237fdae544d3507651fc1e1bbdf18c22bb62
Marshall Greenblatt <magreenblatt@gmail.com>
2017-05-23 15:06:01 +0200

    Add clang-format tooling (issue #2171)

---
+++ tools/clang_util.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2017 The Chromium Embedded Framework Authors. All rights
+# reserved. Use of this source code is governed by a BSD-style license that
+# can be found in the LICENSE file
+
+from exec_util import exec_cmd
+import os
+import sys
+
+# Script directory.
+script_dir = os.path.dirname(__file__)
+root_dir = os.path.join(script_dir, os.pardir)
+
+if sys.platform == 'win32':
+  clang_format_exe = 'buildtools/win/clang-format.exe'
+elif sys.platform == 'darwin':
+  clang_format_exe = 'buildtools/mac/clang-format'
+elif sys.platform.startswith('linux'):
+  clang_format_exe = 'buildtools/linux64/clang-format'
+else:
+  raise Exception("Unsupported platform: %s" % sys.platform)
+
+def clang_format(file_name, file_contents):
+  # -assume-filename is necessary to find the .clang-format file and determine
+  # the language when specifying contents via stdin.
+  result = exec_cmd("%s -assume-filename=%s" % \
+                    (os.path.join(script_dir, clang_format_exe), file_name), \
+                    root_dir, file_contents)
+  if result['out'] != '':
+    output = result['out']
+    if sys.platform == 'win32':
+      # Convert to Unix line endings.
+      output = output.replace("\r", "")
+    return output
+  return None
---
+++ tools/git_util.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2014 The Chromium Embedded Framework Authors. All rights
+# reserved. Use of this source code is governed by a BSD-style license that
+# can be found in the LICENSE file
+
+from exec_util import exec_cmd
+import sys
+
+# Git must be in the system PATH.
+git_exe = 'git'
+
+def get_changed_files(path, hash):
+  """ Retrieves the list of changed files. """
+  if hash == 'unstaged':
+    cmd = "%s diff --name-only" % git_exe
+  elif hash == 'staged':
+    cmd = "%s diff --name-only --cached" % git_exe
+  else:
+    cmd = "%s diff-tree --no-commit-id --name-only -r %s" % (git_exe, hash)
+  result = exec_cmd(cmd, path)
+  if result['out'] != '':
+    files = result['out']
+    if sys.platform == 'win32':
+      # Convert to Unix line endings.
+      files = files.replace('\r\n', '\n')
+    return files.strip().split("\n")
+  return []

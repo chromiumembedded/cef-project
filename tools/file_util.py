d01041331ded707f705134a3d9f87f14d6f66171
Marshall Greenblatt <magreenblatt@gmail.com>
2020-01-14 14:42:39 +0100

    tools: Add Python 3 support (CEF issue #2856)

--- tools/file_util.py
+++ tools/file_util.py
@@ -2,38 +2,41 @@
 # reserved. Use of this source code is governed by a BSD-style license that
 # can be found in the LICENSE file.
 
+from __future__ import absolute_import
 from glob import iglob
+from io import open
 import sys
 
 
 def read_file(name, normalize=True):
   """ Read a file. """
   try:
-    f = open(name, 'r')
-    # read the data
-    data = f.read()
-    if normalize:
-      # normalize line endings
-      data = data.replace("\r\n", "\n")
-    return data
-  except IOError, (errno, strerror):
+    with open(name, 'r', encoding='utf-8') as f:
+      # read the data
+      data = f.read()
+      if normalize:
+        # normalize line endings
+        data = data.replace("\r\n", "\n")
+      return data
+  except IOError as e:
+    (errno, strerror) = e.args
     sys.stderr.write('Failed to read file ' + name + ': ' + strerror)
     raise
-  else:
-    f.close()
 
 
 def write_file(name, data):
   """ Write a file. """
   try:
-    f = open(name, 'w')
-    # write the data
-    f.write(data)
-  except IOError, (errno, strerror):
+    with open(name, 'w', encoding='utf-8') as f:
+      # write the data
+      if sys.version_info.major == 2:
+        f.write(data.decode('utf-8'))
+      else:
+        f.write(data)
+  except IOError as e:
+    (errno, strerror) = e.args
     sys.stderr.write('Failed to write file ' + name + ': ' + strerror)
     raise
-  else:
-    f.close()
 
 
 def get_files(search_glob):
